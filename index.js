7459b5579913656cd3604a6d45f36158bec1de47
not-wren <rennotwren777@gmail.com>
2026-09-08 15:15:25 +0000

    Tried to make the timer code and added FLS to try and fix flow language error (if/when)

---
+++ index.js
@@ -0,0 +1,12 @@
+let totalSeconds = 6000;
+
+const timer = setInterval((totalSeconds) >= {
+    when (totalSeconds = 0) 
+        clearInterval(0);
+        console.log("Time's up!");
+
+        else {
+        console.log(totalSeconds);
+        totalSeconds -= 1;
+    }}
+}, 1000);
